8fe72906d5a2dccf19e0c276808dc2c99b0fc201
Johnny San <johnny.san@gmail.com>
2022-04-12 19:39:32 -0700

    nav-bar looking good

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -7,19 +7,30 @@ import { useSelector } from 'react-redux';
 
 const NavBar = () => {
   const user = useSelector(state => state.session.user);
-
+  console.log(user);
   if (user) {
     return (
       <nav className='nav-bar'>
         <div>
           <NavLink to='/' exact={true} activeClassName='active'>
-            <img src='/garage.png' alt='home' className='home-btn'/>
+            <img src={'/vroom.png'} alt='logo' className='logo'/>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to='/' exact={true} activeClassName='active'>
+            <img src='/garage.png' alt='home' className='home-btn' />
           </NavLink>
         </div>
         <div>
           <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
+            <img src={'/users.png'} alt='users' className='users' />
           </NavLink>
+        </div>
+        <div>
+          <img src={'/cars.png'} alt='gallery' className='gallery' />
+        </div>
+        <div>
+          <img src={user.profile_url} alt='profile' className='profile-pic' />
         </div>
         <div>
           <LogoutButton />
