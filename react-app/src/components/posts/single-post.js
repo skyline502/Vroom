9016721da2f20ba4089b68d7e0601a303951731b
Johnny San <johnny.san@gmail.com>
2022-04-14 22:17:55 -0700

    create comment sort of works

--- react-app/src/components/posts/single-post.js
+++ react-app/src/components/posts/single-post.js
@@ -1,18 +1,45 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useRef, useEffect } from "react";
+import { createAComment, getAllComments } from "../../store/comments";
 import './SinglePost.css'
 
 const SinglePost = () => {
   const currentPost = useSelector(state => state.posts.current);
   const comments = useSelector(state => state.comments.comments);
+  const user = useSelector(state => state.session.user);
   const post_comments = comments.filter(comment => comment.post_id === currentPost.id);
+  const [newComment, setNewComment] = useState('');
+  const dispatch = useDispatch();
 
   const convertDate = (date) => {
     let converted = new Date(date);
     return converted.toLocaleString();
   }
+  const commentsEnd = useRef(null);
+
+  useEffect(() => {
+    commentsEnd.current?.scrollIntoView();
+  }, [dispatch,post_comments])
+
+  const onSubmit = (e) => {
+    if (e.keyCode === 13) {
+      let comment = new FormData();
+      comment.append('post_id', currentPost.id);
+      comment.append('user_id', user.id);
+      comment.append('comment', newComment);
+      let data = dispatch(createAComment(comment));
+      if (data) {
+        setNewComment('');
+      }
+
+      dispatch(getAllComments());
+      console.log(comment, 'I have submitted a new comment......')
+    }
+  }
 
   console.log('in single post', currentPost)
   console.log('current post comments', post_comments)
+  console.log('my comment is...', newComment);
   return (
     <div className="single-post-container">
       <div className="single-post-img">
@@ -28,11 +55,11 @@ const SinglePost = () => {
             <div className="post-name"> {currentPost.user_id.username}</div>
           </div>
           <div className="poster-content">
-              <p>{currentPost.description}</p>
-              <p>{convertDate(currentPost.updated_at)}</p>
+            <p>{currentPost.description}</p>
+            <p>{convertDate(currentPost.updated_at)}</p>
           </div>
         </div>
-        <div className="poster-post-content">
+        <div className="comment-box">
           {post_comments?.map(comment => (
             <div key={comment.id} className='post-contents'>
               <div className="poster-img">
@@ -45,6 +72,24 @@ const SinglePost = () => {
               </div>
             </div>
           ))}
+          <div ref={commentsEnd}></div>
+        </div>
+        <div className="likes">
+          <i className="fas fa-fire" />
+          <label htmlFor="newComment" className="chat">
+            <i className="far fa-comment" />
+          </label>
+        </div>
+        <div className="create-comment">
+          <input
+            type='text'
+            id='newComment'
+            value={newComment}
+            placeholder='Add a comment...'
+            onChange={e => setNewComment(e.target.value)}
+            className='crt-cmt-input'
+            onKeyDown={onSubmit}
+          />
         </div>
       </div>
     </div>
--- react-app/src/store/comments.js
+++ react-app/src/store/comments.js
@@ -1,8 +1,14 @@
 const GET_COMMENTS = 'comments/GET_COMMENTS';
+const CREATE_COMMENT = 'comments/CREATE_COMMENT';
 
 const getComments = (comments) => ({
   type: GET_COMMENTS,
   comments
+});
+
+const createComment = (comment) => ({
+  type: CREATE_COMMENT,
+  comment
 });
 
 export const getAllComments = () => async dispatch => {
@@ -17,14 +23,39 @@ export const getAllComments = () => async dispatch => {
   return response;
 }
 
+export const createAComment = (comment) => async dispatch => {
+  console.log('I have reached the store for create a comment....')
+  const response = await fetch(`/api/comments/`, {
+    method: 'POST',
+    body: comment,
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(createComment(data));
+    return null;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ['An error occurred. Please try again.']
+  }
+}
+
 
 const commentsReducer = (state = {comments: [], current: {}}, action) => {
+  let newState = {...state}
   switch(action.type) {
     case GET_COMMENTS:
       return {
         ...state,
         comments: [...action.comments.comments]
       }
+    case CREATE_COMMENT: {
+      return newState;
+    }
     default:
       return state;
   }
