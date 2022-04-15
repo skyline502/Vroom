1b32dbf35a22c6bada03ce5ba45baa71d00e9d7f
Johnny San <johnny.san@gmail.com>
2022-04-15 09:05:51 -0700

    fixed link thing

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import { showModal, setCurrentModal } from '../../store/modal';
 import './LoginForm.css'
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
 import { hideModal } from '../../store/modal';
 import './LoginForm.css'
