3ac368113ed418c32497bcbc6622d77779ff155c
Johnny San <johnny.san@gmail.com>
2022-04-22 10:56:00 -0700

    looks good

--- react-app/src/components/options/index.js
+++ react-app/src/components/options/index.js
@@ -2,7 +2,7 @@ import { hideModal, setCurrentModal, showModal } from "../../store/modal";
 import { setPost } from "../../store/posts";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getAllPosts, deleteAPost, getOnePost } from "../../store/posts";
+import { getAllPosts, deleteAPost } from "../../store/posts";
 import EditPostForm from "../posts/edit-post-modal/edit-post";
 import './Options.css';
 import SinglePost from '../posts/single-post';
@@ -51,10 +51,14 @@ const Options = () => {
         {user.id === currentPost.user_id?.id ? (
           <div className='options-buttons'>
             <div onClick={() => showSinglePost(currentPost)}><p>Go to post</p></div>
-            <div><button onClick={() => showEditForm(currentPost)}><i className="fas fa-wrench"></i>Edit Post</button></div>
-            <div><button onClick={() => deletePost(currentPost.id)} style={{color: 'red'}}><i className="fas fa-trash-alt"></i>Delete Post</button></div>
+            <div><button onClick={() => showEditForm(currentPost)}>Edit Post</button></div>
+            <div><button onClick={() => deletePost(currentPost.id)} style={{ color: 'red' }}>Delete Post</button></div>
+            <p onClick={() => dispatch(hideModal())}>cancel</p>
           </div>
-        ) : <div onClick={() => showSinglePost()}><p>Go to post</p></div>
+        ) : <div className="options-buttons">
+          <p onClick={() => showSinglePost()}>Go to post</p>
+          <p onClick={() => dispatch(hideModal())}>cancel</p>
+        </div>
         }
       </div>
     </div>
